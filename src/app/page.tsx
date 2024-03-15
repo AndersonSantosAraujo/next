28cbebc6dd3ebaf82abebe9a0d96c278eb068bec
Anderson Araujo <anderson.araujo@trinto.com.br>
2024-03-14 22:30:47 -0300

    feat: Scripts (#0505)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import Script from "next/script";
+
 export default async function HomePage() {
   return (
     <main>
       <h1>Home</h1>
+      <Script
+        id="script-legal"
+        src="https://api.origamid.online/scripts/idade-legal.min.js"
+        strategy="afterInteractive"
+      />
+      <Script id="script-test">{`console.log('Teste')`}</Script>
     </main>
   );
 }
